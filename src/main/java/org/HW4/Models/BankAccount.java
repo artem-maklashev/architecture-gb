72b39d820d591e8c306acb761729c7ecd57b3272
Artem Maklashev <artem@maklashev.ru>
2024-08-17 21:25:14 +0400

    first

---
+++ src/main/java/org/HW4/Models/BankAccount.java
@@ -0,0 +1,25 @@
+package org.HW4.Models;
+
+import javax.smartcardio.Card;
+
+/**
+ * Модель банковского аккаунта
+ */
+public class BankAccount {
+
+    private int balance;
+    private Card card;
+
+
+    public BankAccount() {
+        balance = 1000;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount {" +
+                " card= " + (String.format("%016d", card)) +
+                ", balance= " + balance +
+                " }";
+    }
+}
