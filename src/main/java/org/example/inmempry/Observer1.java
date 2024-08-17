3bd3d98fb655f619397206e13449e5048dbf63d5
Artem Maklashev <artem@maklashev.ru>
2024-08-17 15:32:38 +0400

    sem4 init

--- src/main/java/org/example/inmempry/Observer1.java
+++
@@ -1,8 +0,0 @@
-package org.example.inmempry;
-
-public class Observer1 implements IModelChangedObserver{
-    @Override
-    public void applyUpdateModel() {
-        System.out.println("Произошло добавление в хранилище - обозреватель №1");
-    }
-}
