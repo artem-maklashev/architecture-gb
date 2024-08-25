27c58da6d841223c137b6d5517de0fb5effb63b9
Artem Maklashev <artem@maklashev.ru>
2024-08-25 10:55:11 +0400

    sem6 init

--- src/main/java/org/example/inmempry/IModelChanger.java
+++
@@ -1,7 +0,0 @@
-package org.example.inmempry;
-
-public interface IModelChanger {
-    void notifyChange();
-    void registerModelChanger(IModelChangedObserver observer);
-    void unregisterModelChanger(IModelChangedObserver observer);
-}
